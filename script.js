aa3c0f3aeb015bc912913c2992064ecca2bb20c8
Tanoy Bosh <tanoybosh@gmail.com>
2024-03-21 15:33:37 +0530

    4

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
+const texts = [
+    "Hello World!",
+    "I'm Tanoy Bosh",
+    "a Software Engineer"
+];
+
+let textIndex = 0;
+let charIndex = 0;
+let currentText = '';
+let typingTimeout;
+
+function typeWriter() {
+    if (textIndex === texts.length) return; // Stop if all lines have been typed
+    
+    currentText = texts[textIndex];
+    if (charIndex < currentText.length) {
+        document.querySelectorAll('.type')[textIndex].textContent += currentText.charAt(charIndex);
+        charIndex++;
+        typingTimeout = setTimeout(typeWriter, 100); // Speed of typing (milliseconds)
+    } else {
+        charIndex = 0;
+        textIndex++;
+        typingTimeout = setTimeout(typeWriter, 1000); // Delay between lines (milliseconds)
+    }
+}
+
+typeWriter(); // Start typing when the page is loaded
+
 // JavaScript to dynamically set the current year
 document.getElementById("currentYear").textContent = new Date().getFullYear();
